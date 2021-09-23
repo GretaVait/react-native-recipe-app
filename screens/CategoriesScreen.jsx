16278ece66141dd93e8a34f5e086b2b78e88e4dc
GretaVait895 <gretavait@gmail.com>
2021-09-23 10:33:39 +0300

    add categories to categories screen

--- screens/CategoriesScreen.jsx
+++ screens/CategoriesScreen.jsx
@@ -1,29 +1,54 @@
 import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet, FlatList } from 'react-native'
 import { CommonActions } from '@react-navigation/native'
+import { CATEGORIES } from '../data/dummy-data'
+import { ScrollView } from 'react-native-gesture-handler'
 
 const CategoriesScreen = ({ navigation }) => {
 
+  const renderGridItem = (itemData) => {
+    return (
+      <View style={styles.gridItem}>
+        <Text>{itemData.item.title}</Text>
+      </View>
+    )
+  }
+
   return (
+    // <FlatList 
+    //   contentContainerStyle={styles.list}
+    //   data={CATEGORIES} 
+    //   renderItem={renderGridItem} 
+    // />
     <View style={styles.screen}>
-      <Text>The categories screen</Text>
-      <Button 
-        title="Go to Category Meals" 
-        onPress={() =>
-          navigation.dispatch(
-            CommonActions.navigate({
-              name: 'Category Meals',
-            })
-          )
+      <ScrollView contentContainerStyle={styles.list}>
+        {
+          CATEGORIES.map((item) => (
+            <View style={styles.gridItem} key={item.id}>
+              <Text>{item.title}</Text>
+            </View>
+          ))
         }
-      />
+      </ScrollView>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   screen: {
-    flex: 1
+    flex: 1,
+    paddingVertical: 32,
+    paddingHorizontal: 16
+  },
+  list: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  gridItem: {
+    margin: 8,
+    padding: 16,
+    backgroundColor: 'green',
+    borderRadius: 8
   }
 })
 
